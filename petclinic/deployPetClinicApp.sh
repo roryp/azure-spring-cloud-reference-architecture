ed45100f7b10a6928f147b2b3d564c253832f524
Rory Preddy <>
2021-04-13 12:07:51 +0200

    shell script

--- petclinic/deployPetClinicApp.sh
+++ petclinic/deployPetClinicApp.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 
 # ==== Must Cusomize the below for your environment====
-subscription=''
-resource_group=''
-region=''
-spring_cloud_service=''
-mysql_server_name=''
-mysql_server_admin_name=''
-mysql_server_admin_password=''
+subscription='296f38ef-7319-4a2b-ab48-2f38cb21966e'
+resource_group='springcloudrpza'
+region='westeurope'
+spring_cloud_service='springcloudrpza'
+mysql_server_name='springcloudrpza'
+mysql_server_admin_name='adminrpza'
+mysql_server_admin_password='@zureIsTheDevil01'
+
 
 #Add Required extensions
 az extension add --name spring-cloud
 
+#set variables
+DEVBOX_IP_ADDRESS=$(curl ifconfig.me)
+
 #Create directory for github code
-cd /c
+cd /Users/rorypreddy/dev
 mkdir source-code
 cd source-code
 
 #Clone GitHub Repo
 git clone https://github.com/azure-samples/spring-petclinic-microservices
 cd spring-petclinic-microservices
-/c/ProgramData/chocolatey/lib/maven/apache-maven-3.6.3/bin/mvn clean package -DskipTests -Denv=cloud
+mvn clean package -DskipTests -Denv=cloud
 
 
 # ==== Service and App Instances ====
@@ -32,23 +36,48 @@ visits_service='visits-service'
 
 
 # ==== JARS ====
-api_gateway_jar='/c/source-code/spring-petclinic-microservices/spring-petclinic-api-gateway/target/spring-petclinic-api-gateway-2.3.6.jar'
-admin_server_jar='/c/source-code/spring-petclinic-microservices/spring-petclinic-admin-server/target/spring-petclinic-admin-server-2.3.6.jar'
-customers_service_jar='/c/source-code/spring-petclinic-microservices/spring-petclinic-customers-service/target/spring-petclinic-customers-service-2.3.6.jar'
-vets_service_jar='/c/source-code/spring-petclinic-microservices/spring-petclinic-vets-service/target/spring-petclinic-vets-service-2.3.6.jar'
-visits_service_jar='/c/source-code/spring-petclinic-microservices/spring-petclinic-visits-service/target/spring-petclinic-visits-service-2.3.6.jar'
+api_gateway_jar='/Users/rorypreddy/dev/source-code/spring-petclinic-microservices/spring-petclinic-api-gateway/target/spring-petclinic-api-gateway-2.3.6.jar'
+admin_server_jar='/Users/rorypreddy/dev/source-code/spring-petclinic-microservices/spring-petclinic-admin-server/target/spring-petclinic-admin-server-2.3.6.jar'
+customers_service_jar='/Users/rorypreddy/dev/source-code/spring-petclinic-microservices/spring-petclinic-customers-service/target/spring-petclinic-customers-service-2.3.6.jar'
+vets_service_jar='/Users/rorypreddy/dev/source-code/spring-petclinic-microservices/spring-petclinic-vets-service/target/spring-petclinic-vets-service-2.3.6.jar'
+visits_service_jar='/Users/rorypreddy/dev/source-code/spring-petclinic-microservices/spring-petclinic-visits-service/target/spring-petclinic-visits-service-2.3.6.jar'
 
 # ==== MYSQL INFO ====
-mysql_server_full_name="${mysql_server_name}.privatelink.mysql.database.azure.com"
+mysql_server_full_name="${mysql_server_name}.mysql.database.azure.com"
 mysql_server_admin_login_name="${mysql_server_admin_name}@${mysql_server_full_name}"
 mysql_database_name='petclinic'
 
 
-cd /c/source-code/spring-petclinic-microservices
+cd /Users/rorypreddy/dev/source-code/spring-petclinic-microservices
 
 az login
 az account set --subscription ${subscription}
 
+az group create --name ${resource_group} --location ${region}
+az mysql server create \
+    --resource-group ${resource_group} \
+    --name ${mysql_server_name} \
+    --location ${region} \
+    --sku-name B_Gen5_1 \
+    --storage-size 5120 \
+    --admin-user ${mysql_server_admin_name} \
+    --admin-password ${mysql_server_admin_password}
+az mysql server firewall-rule create \
+    --resource-group ${resource_group} \
+    --name ${mysql_server_name}-database-allow-local-ip \
+    --server ${mysql_server_name} \
+    --start-ip-address ${DEVBOX_IP_ADDRESS} \
+    --end-ip-address ${DEVBOX_IP_ADDRESS}
+az mysql server firewall-rule create \
+    --resource-group ${resource_group} \
+    --name allAzureIPs \
+    --server ${mysql_server_name} \
+    --start-ip-address 0.0.0.0 --end-ip-address 0.0.0.0
+az spring-cloud create \
+    -g ${resource_group} \
+    -n ${spring_cloud_service} \
+    --sku standard \
+    --enable-java-agent
 az configure --defaults group=${resource_group} location=${region} spring-cloud=${spring_cloud_service}
 az spring-cloud config-server set --config-file application.yml --name ${spring_cloud_service}
 
